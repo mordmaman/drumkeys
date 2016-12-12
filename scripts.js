ba645090db05086c1fd50055dc38abaf33de58d4
Mord Maman <jaime.mord.maman@gmail.com>
2016-12-12 16:21:15 +0000

    adds audio tage to d div and .play function

--- scripts.js
+++ scripts.js
@@ -5,5 +5,6 @@ window.addEventListener("keydown", checkKeyPressed, false);
 function checkKeyPressed(e){
     if (e.keyCode == "68") {
         console.log("The 'd' key is pressed.");
+        document.getElementById('audio').play();
     }
 }
